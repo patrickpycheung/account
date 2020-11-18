b4c6d979f93477e2088e9b51b782918dfdca959a
Patrick Cheung <patrick_cheung23@hotmail.com>
2020-11-18 15:33:33 +1100

    Amend the sample data for H2 DB.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,9 @@
 INSERT INTO ACCOUNT (cust_id, account_num, account_name, account_type, balance_date, currency, opening_available_balance) VALUES
-  (1, 585309209, 'SGSavings726', 'Savings', TIMESTAMP '2018-11-08 00:00:00', 'SGD', 84327.51),
-  (2, 791066619, 'AUSavings933', 'Savings', TIMESTAMP '2018-11-08 00:00:00', 'AUD', 88005.93);
+  (1111111111, 1111111111, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'Savings', TIMESTAMP '2020-11-01 11:01:01', 'SGD', 99999.99),
+  (2, 2, 'B', 'Savings', TIMESTAMP '2020-11-02 11:02:02', 'AUD', 0.0),
+  (1111111111, 3333333333, 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCC', 'Current', TIMESTAMP '2020-11-03 11:03:03', 'USD', 99999.99);
 
 INSERT INTO TRANSACTION (account_num, account_name, value_date, currency, debit_amt, credit_amt, debit_credit, transaction_narrative) VALUES
-  (585309209, 'Current Account', TIMESTAMP '2012-01-12 00:00:00', 'SGD', null, 9540.98, 'Credit', 'Rent payment'),
-  (1232223212, 'Savings Account', TIMESTAMP '2012-01-12 00:00:00', 'USD', 1234.56, null, 'Debit', null);
+  (1111111111, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', TIMESTAMP '2012-11-01 11:01:01', 'SGD', null, 99999.99, 'Credit', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'),
+  (2, 'Savings Account', TIMESTAMP '2012-11-02 11:02:02', 'USD', 0.0, null, 'Debit', null),
+  (1111111111, 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCC', TIMESTAMP '2012-11-03 11:03:03', 'USD', 99999.99, null, 'Debit', 'CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC');
