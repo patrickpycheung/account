e8c42fea39c562b0d6c720e15ccb2c4644fa854b
Patrick Cheung <patrick_cheung23@hotmail.com>
2020-11-19 19:45:20 +1100

    Amend validations on the model class "Customer".

--- src/main/java/com/somecompany/account/model/Customer.java
+++ src/main/java/com/somecompany/account/model/Customer.java
@@ -1,6 +1,10 @@
 package com.somecompany.account.model;
 
-import javax.validation.constraints.NotNull;
+import javax.persistence.Column;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
@@ -8,8 +12,11 @@ import lombok.Data;
 @Data
 public class Customer {
 
-	@NotNull(message = "Customer ID cannot be null")
-	@Size(min = 1, max = 10, message = "Customer ID must have length between 1 and 10")
+	@Column(name = "cust_id")
+	@NotEmpty(message = "Customer ID cannot be null nor empty")
+	@Pattern(regexp = "^[0-9]+$", message = "Customer ID must be a number")
+	@Min(value = 1L, message = "Customer ID must not be less than 1")
+	@Max(value = 9999999999L, message = "Customer ID must not be larger than 9999999999")
 	private long custId;
 
 	@Size(min = 0, max = 30, message = "Customer name must have length between 1 and 30")
